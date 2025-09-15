4b41231c16f0c4b9bea145bc1b303fb784f53771
unclebinary1001 <nzundamahlangu@gmail.com>
2025-09-15 08:15:55 -0700

    add other 'drinks' and update release apk

--- app/src/main/java/com/example/beveragetracker/MainActivity.kt
+++ app/src/main/java/com/example/beveragetracker/MainActivity.kt
@@ -6,15 +6,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var historyButton: ImageButton
+    private lateinit var emptyIcon: ImageView
     private lateinit var waterIcon: ImageView
     private lateinit var coffeeIcon: ImageView
     private lateinit var teaIcon: ImageView
     private lateinit var juiceIcon: ImageView
+    private lateinit var milkIcon: ImageView
+    private lateinit var hotCocoaIcon: ImageView
+    private lateinit var energyIcon: ImageView
+    private lateinit var smoothieIcon: ImageView
+    private lateinit var sodaIcon: ImageView
+    private lateinit var wineIcon: ImageView
+    private lateinit var beerIcon: ImageView
     private lateinit var historyManager: HistoryManager // Change from beverageDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +35,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, HistoryActivity::class.java))
         }
 
+        emptyIcon = findViewById(R.id.empty_icon)
         waterIcon = findViewById(R.id.water_icon)
         coffeeIcon = findViewById(R.id.coffee_icon)
         teaIcon = findViewById(R.id.tea_icon)
         juiceIcon = findViewById(R.id.juice_icon)
+        milkIcon = findViewById(R.id.milk_icon)
+        hotCocoaIcon = findViewById(R.id.hot_cocoa_icon)
+        energyIcon = findViewById(R.id.energy_icon)
+        smoothieIcon = findViewById(R.id.smoothie_icon)
+        sodaIcon = findViewById(R.id.soda_icon)
+        wineIcon = findViewById(R.id.wine_icon)
+        beerIcon = findViewById(R.id.beer_icon)
 
+
+
+        emptyIcon.setOnClickListener {
+            showInputDialog("Empty")
+        }
         waterIcon.setOnClickListener {
             showInputDialog("Water")
         }
@@ -44,6 +64,27 @@ class MainActivity : AppCompatActivity() {
         }
         juiceIcon.setOnClickListener {
             showInputDialog("Juice")
+        }
+        milkIcon.setOnClickListener {
+            showInputDialog("Milk")
+        }
+        hotCocoaIcon.setOnClickListener {
+            showInputDialog("Hot Cocoa")
+        }
+        energyIcon.setOnClickListener {
+            showInputDialog("Energy")
+        }
+        smoothieIcon.setOnClickListener {
+            showInputDialog("Smoothie")
+        }
+        sodaIcon.setOnClickListener {
+            showInputDialog("Soda")
+        }
+        wineIcon.setOnClickListener {
+            showInputDialog("Wine")
+        }
+        beerIcon.setOnClickListener {
+            showInputDialog("Beer")
         }
     }
 
